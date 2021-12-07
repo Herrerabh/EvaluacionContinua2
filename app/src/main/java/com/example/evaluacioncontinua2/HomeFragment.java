c31370371fab8f81b481233f9bc88099bb2f4eb6
Eduardo <eduardobherrera@hotmail.com>
2021-12-07 17:11:52 -0500

    Login

---
+++ app/src/main/java/com/example/evaluacioncontinua2/HomeFragment.java
@@ -0,0 +1,19 @@
+package com.example.evaluacioncontinua2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/evaluacioncontinua2/NavigationHost.java
@@ -0,0 +1,7 @@
+package com.example.evaluacioncontinua2;
+
+import androidx.fragment.app.Fragment;
+
+public interface NavigationHost {
+    void navigateTo(Fragment fragment, boolean addToBackstack);
+}
